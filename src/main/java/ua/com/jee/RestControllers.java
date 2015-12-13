8a57082dfd2738777987d77aaed442ccebf2fe8f
dguly <digulUrb@gmail.com>
2015-12-13 21:24:38 +0200

    Added db filling with admin user and check if user existing in db

--- src/main/java/ua/com/jee/RestControllers.java
+++ src/main/java/ua/com/jee/RestControllers.java
@@ -2,6 +2,7 @@ package ua.com.jee;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,17 +17,29 @@ public class RestControllers {
     @Autowired
     private UserEntityRepository repository;
 
+    private boolean isDbFilled = false;
+
+
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public HttpStatus login(@RequestParam("userName") String name,
+    public ResponseEntity login(@RequestParam("userName") String name,
                             @RequestParam("password") String password) {
+
+        if (!isDbFilled) {
+            fillDb();
+            isDbFilled = true;
+        }
 
         UserEntity userEntity = repository.findByNameIgnoringCase(name);
 
         if (ObjectUtils.isEmpty(userEntity)) {
-            return HttpStatus.BAD_REQUEST;
+            return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
         } else {
             System.out.println(userEntity.toString());
-            return HttpStatus.OK;
+            return new ResponseEntity<String>(HttpStatus.OK);
         }
+    }
+
+    private void fillDb() {
+        repository.save(new UserEntity("admin", "admiadded "));
     }
 }
--- src/test/java/CoreDbFuncTest.java
+++ src/test/java/CoreDbFuncTest.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ua.com.jee.Application;
+import ua.com.jee.entity.UserEntity;
 import ua.com.jee.repository.UserEntityRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
