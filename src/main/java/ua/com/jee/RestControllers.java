894f90ff8ba028c121931a8ffcf0a634448ca0cd
dguly <digulUrb@gmail.com>
2015-12-13 20:49:14 +0200

    Implemented request and response handling, minor refactoring

--- src/main/java/ua/com/jee/RestControllers.java
+++ src/main/java/ua/com/jee/RestControllers.java
@@ -1,15 +1,21 @@
 package ua.com.jee;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ua.com.jee.entity.UserEntity;
 
 @RestController
 public class RestControllers {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login() {
-        return "Login page!";
+    public HttpStatus login(@RequestParam("userName") String name,
+                            @RequestParam("password") String password) {
+        UserEntity userEntity = new UserEntity(name, password);
+        System.out.println(userEntity.toString());
+        return HttpStatus.OK;
     }
 
 }
--- src/main/webapp/script/index.js
+++ src/main/webapp/script/index.js
@@ -7,10 +7,10 @@ $(document).ready(() => {
         let userName = $("#login_form input[type=text]").val();
         let password = $("#login_form input[type=password]").val();
 
-        $.post("/login", {userName, password}, (response) => {
-            if (response === "Success") {
+        $.post("/login", {userName, password}, (data, status, response) => {
+            if (response.status === 200) {
                 $("#login_form").hide();
-                $("#confirm_form").show();
+                $("#confirm_login_form").show();
             } else {
                 alert("WTF?")
             }
